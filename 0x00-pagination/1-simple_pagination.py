ccee6f1d5f8063c3fe665df2522f11443c00f972
Leornadia <leornadiajose@gmail.com>
2024-08-18 08:55:13 -0700

    1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,6 +8,15 @@ import math
 from typing import List, Tuple
 
 
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Return a tuple of start and end indexes for pagination
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -27,14 +36,6 @@ class Server:
 
         return self.__dataset
 
-    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
-        """
-        Return a tuple of start and end indexes for pagination
-        """
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-        return (start_index, end_index)
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         Retrieve a specific page of data from the dataset.
@@ -42,7 +43,7 @@ class Server:
         assert isinstance(page, int) and page > 0, "page must be a positive integer"
         assert isinstance(page_size, int) and page_size > 0, "page_size must be a positive integer"
 
-        start, end = self.index_range(page, page_size)
+        start, end = index_range(page, page_size)
         data = self.dataset()
 
         if start >= len(data):

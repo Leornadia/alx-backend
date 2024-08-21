657ccf9618a507a7d0523ef1a13ddecb39704951
Leornadia <leornadiajose@gmail.com>
2024-08-21 08:52:05 -0700

    2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -10,7 +10,7 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """Initialize the class with parent's init method"""
         super().__init__()
-        self.last_key = None  # To keep track of the last key added
+        self.keys_order = []  # To track the order of keys
 
     def put(self, key, item):
         """Add an item in the cache"""
@@ -20,21 +20,20 @@ class LIFOCache(BaseCaching):
         if key in self.cache_data:
             # Update the item if the key already exists
             self.cache_data[key] = item
+            self.keys_order.remove(key)
+            self.keys_order.append(key)
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 # Remove the last item added (LIFO)
-                if self.last_key is not None:
-                    del self.cache_data[self.last_key]
-                    print(f"DISCARD: {self.last_key}")
+                last_key = self.keys_order.pop()
+                del self.cache_data[last_key]
+                print(f"DISCARD: {last_key}")
+
             # Add the new item to the cache
             self.cache_data[key] = item
-
-        # Update the last key to this key
-        self.last_key = key
+            self.keys_order.append(key)
 
     def get(self, key):
         """Get an item by key"""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
 

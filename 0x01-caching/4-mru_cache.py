9d0203cc4b14716b961b4aae473281321e2a28fe
Leornadia <leornadiajose@gmail.com>
2024-08-21 09:30:02 -0700

    4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -9,31 +9,35 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """Initialize the class"""
         super().__init__()
-        self.mru_order = []  # Track the order of access (MRU order)
+        self.usage_order = []  # List to track the order of key usage
 
     def put(self, key, item):
         """Add an item in the cache"""
         if key is None or item is None:
             return
 
+        # If the key is already in the cache, remove it to update its position later
+        if key in self.cache_data:
+            self.usage_order.remove(key)
+
         # Add or update the cache data
         self.cache_data[key] = item
-
-        # Update mru_order (handle existing key and append)
-        if key in self.mru_order:
-            self.mru_order.remove(key)
-        self.mru_order.append(key)
+        self.usage_order.append(key)
 
         # Handle MRU removal if necessary
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            mru_key = self.mru_order.pop(0)
+            # The most recently used item is the last one in usage_order
+            mru_key = self.usage_order.pop(-2)  # Discard the second last item in the list
             del self.cache_data[mru_key]
             print(f"DISCARD: {mru_key}")
 
     def get(self, key):
         """Get an item by key"""
-        if key in self.cache_data:
-            # Update mru_order (remove and re-append)
-            self.mru_order.remove(key)
-            self.mru_order.append(key)
-        return self.cache_data.get(key, None)
+        if key is None or key not in self.cache_data:
+            return None
+
+        # Update the usage order because this key was accessed
+        self.usage_order.remove(key)
+        self.usage_order.append(key)
+        return self.cache_data[key]
+

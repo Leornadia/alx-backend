de193793f0d8e9a7887c0a44942510b6b6ef1e81
Leornadia <leornadiajose@gmail.com>
2024-08-27 09:53:17 -0700

    1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
-""" 1. Initialize Flask app """
+"""Basic Flask app with Babel"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
-class Config(object):
-    """ Configuration class for Flask app. """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config:
+    """Config class for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app) 
 app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
-@app.route("/")
-def welcome() -> str:
-    """ / page """
+@app.route('/')
+def index():
+    """Route for index page"""
     return render_template('1-index.html')
 
 

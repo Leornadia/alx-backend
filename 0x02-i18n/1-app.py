0f6dad3f7b5eeec05d2266598d793d4ce3e6b198
Leornadia <leornadiajose@gmail.com>
2024-08-27 12:20:04 -0700

    1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
-"""Basic Flask app with Babel"""
+""" 1. Basic Flask app with Babel integration. """
 from flask import Flask, render_template
 from flask_babel import Babel
 
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 class Config:
-    """Config class for Flask app"""
+    """Configuration class for Babel."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
-app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
+
 babel = Babel(app)
 
-
-@app.route('/')
-def index():
-    """Route for index page"""
+@app.route("/")
+def welcome():
+    """Renders the home page with a welcome message."""
     return render_template('1-index.html')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+

088bf3723e57fde44980de29077632910bdc4c6f
Leornadia <leornadiajose@gmail.com>
2024-08-27 08:46:12 -0700

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-"""Basic Flask app"""
-
+""" 0. Basic Flask app. """
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route('/')
-def index():
-    """Route for the home page"""
+
+@app.route("/")
+def welcome():
+    """ / page """
     return render_template('0-index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
